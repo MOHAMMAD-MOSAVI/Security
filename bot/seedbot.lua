8b1c035d6f1e322727adaadee9d90b34a436d5d2
MOHAMMAD-MOSAVI <www.mamad1394@gmail.com>
2016-01-08 19:28:12 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {160957134,tonumber(our_id)},--Sudo users
+    sudo_users = {160957134,131218437,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
